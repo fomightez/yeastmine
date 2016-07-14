1f7532f7bfdc6c0978b8d28453f67c63c8017e33
Wayne's Bioinformatics Code Portal <fomightez@gmail.com>
2016-07-14 16:46:39 -0400

    change file naming for conversion data
    change file naming for conversion data storage to automagically get information about month and year in name and in text content.

--- geneID_list_to_systematic_names.py
+++ geneID_list_to_systematic_names.py
@@ -2,6 +2,9 @@
 
 # see `Trying to convert Shafi's list to something that works in T-profiler.md` for info about this script
 
+# To do: make so input file name isn't hard coded into script, and the output
+# file name is based on the input.
+
 
 #*******************************************************************************
 ##################################
@@ -13,8 +16,9 @@ input_file_name = "culled_extracted_geneIDs_and_log2change.txt"
 
 output_file_name = "systematicIDs_and_log2change.txt"
 
-stored_conversion_data_file = "yeastmine_conversion_dataMay2016.py" # data to
-# save for subsequent use or to use from previous runs.
+stored_conversion_data_file_prefix = "yeastmine_conversion_data" #
+# prefix for file name to save data for subsequent use or to use  from previous
+# runs. Approach like this first worked out in `make simulated_yeast_gene_set.py`
 
 discarding_non_orfs = False   # Only keep ORFs? Disarding non-ORFS will mean those
 # like NME1 that don't have a standard name beginnning in `Y`, like YDR190C or
@@ -54,21 +58,57 @@ import os
 
 ###---------------------------HELPER FUNCTIONS---------------------------------###
 
-def header_text_note_maker():
+def generate_stored_data_file_name(file_name_prefix):
+    '''
+    Takes a file name as an argument and returns string for the name of the
+    output file. The generated name is based on the original file
+    name.
+
+    Specific example
+    ================
+    Calling function in June 2016 with
+        ("yeastmine_gene_data_for_creating_random_set")
+    returns
+        "yeastmine_gene_data_for_creating_random_setJune2016.py"
+
+    It also returns month and year to use in the data file text.
+
+    This was originally developed in `make simulated_yeast_gene_set.py`
+
+
+    '''
+    # prepare to timestamp the name of file to save
+    import datetime
+    now = datetime.datetime.now()
+    # see http://www.saltycrane.com/blog/2008/06/how-to-get-current-date-and-time-in/
+    # for formatting options simplified
+    return file_name_prefix + now.strftime("%B%Y") +".py", now.strftime(
+        "%B"), now.strftime("%Y")
+
+def header_text_note_maker(month, year):
     '''
     function generates text that puts description(header) for list entries in
     gene_data_list
+
+    month and year are provided from the calling function to make header better
     '''
-    header_text = "# These were all the genes in YeastMine at the time the corresponding data was analyzed.\n# The values in the list for each gene are as follows:\n"
-    header_text += "# primaryIdentifier, secondaryIdentifier, symbol, name, proteins.symbol, sgdAlias, featureType, description\n"
+    header_text = "# These were the identifiers for all the " + str(
+        features_in_genome ) +" yeast sequenceFeatures in YeastMine "+ month + " " + year +".\n# The values in the list for each are as follows:\n"
+    header_text += "# primaryIdentifier, secondaryIdentifier, symbol, name, sgdAlias, featureType, description\n"
     return header_text
 
 def generate_data_file(the_data_list):
     '''
     function takes gene data list from YeastMine and saves it as script-like file
     '''
-    data_file_stream = open(stored_conversion_data_file , "w")
-    data_file_stream.write(header_text_note_maker())
+    stored_gene_data_file, month, year  = generate_stored_data_file_name(
+        stored_conversion_data_file_prefix) # see next line for info on month & year.
+    # Month and year are first used in the function just called and so instead
+    # of calling that import and commands again, just passing back.
+
+
+    data_file_stream = open(stored_gene_data_file , "w")
+    data_file_stream.write(header_text_note_maker(month, year))
     data_file_stream.write('gene_data_list = ' + repr(gene_data_list ) + "\n")
     data_file_stream.close()
     sys.stderr.write( "\b \nGene data stored for future use.\n") # `\b ` in the
@@ -78,7 +118,21 @@ def generate_data_file(the_data_list):
 ###--------------------------END OF HELPER FUNCTIONS---------------------------###
 
 
+# Use previously saved list of yeast gene info, or if none exists contact YeastMine
+# and get a list of the genes
 
+# To check for a file that begins with the prefix make a list of the files in
+# the folder with the script and check if one of them begins with the prefix text.
+# Then use that to get name if any match by taking first one because there never
+# will be more than one unless debugging. Set to something that couldn't
+# possibly match anything in folder otherwise so can use
+# `os.path.isfile(stored_gene_data_file)` without having to first check if `stored_gene_data_file` id defined.
+# Line below based on Marc L's answer at http://stackoverflow.com/questions/15312953/choose-a-file-starting-with-a-given-string
+stored_gene_data_file_possible_list = [filename for filename in os.listdir('.') if (filename.startswith(stored_conversion_data_file_prefix )) and (not filename.endswith('.pyc'))] # the `not filename.endswith('pyc')` is to exclude getting name of compiled version
+if stored_gene_data_file_possible_list:
+    stored_conversion_data_file = stored_gene_data_file_possible_list[0]
+else:
+    stored_conversion_data_file = "DOESN'T EXIST and this is an impossible name to never match a file"
 
 if os.path.isfile(stored_conversion_data_file) :
     # If data for conversion table already saved, used that to make dictionary.
@@ -158,6 +212,7 @@ else:
     query.add_constraint("featureType", "=", "intein_encoding_region", code = "Y")
     query.add_constraint("organism.name", "=", "Saccharomyces cerevisiae", code = "T")
     query.set_logic("(A or B or C or D or E or F or G or H or I or J or K or L or M or N or O or P or Q or R or S or X or Y or Z) and T and U and (V or W)")
+    # note for logic to get closer to a real set of yeast genes see `make simulated_yeast_gene_set.py` and also see the filer added on line 331 of that script
 
 
     # Now almost ready to make the dictionary
@@ -183,7 +238,7 @@ else:
             # Collect the data as a list of lists for possible future use and/or speeding up runs
             gene_data_list.append([row["primaryIdentifier"],
                 row["secondaryIdentifier"], row["symbol"], row["name"],
-                row["proteins.symbol"],  row["sgdAlias"], row["featureType"],
+                row["sgdAlias"], row["featureType"],
                 row["description"]])
 
     else:
@@ -197,11 +252,14 @@ else:
             # Collect the data as a list of lists for possible future use and/or speeding up runs
             gene_data_list.append([row["primaryIdentifier"],
                 row["secondaryIdentifier"], row["symbol"], row["name"],
-                row["proteins.symbol"],  row["sgdAlias"], row["featureType"],
+                row["sgdAlias"], row["featureType"],
                 row["description"]])
             #every 200 lines print
             if (rows_processed % 200 == 0):
                 sys.stderr.write(".")
+
+    # assign `rows_processed` to something more meaningful
+    features_in_genome = rows_processed
 
     # Save the data in gene_data_list for possibe future use or for saving time.
     # This will allow storage if the conversion data. This will serve two
